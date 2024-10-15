50089e9a4c894e7bbede0a88de4d599674b6f900
Kaspar Bunne <kasparbunne@gmail.com>
2024-10-15 13:57:52 +0200

    add functions in clustering.py

--- src/prevelop/clustering.py
+++ src/prevelop/clustering.py
@@ -8,6 +8,9 @@ import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram
 from collections import Counter
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from yellowbrick.cluster import SilhouetteVisualizer
 
 
 def gower_distance(data):
@@ -62,3 +65,53 @@ def first_clusters(data, distance_matrix, nr_clusters):
         print('Cluster:', i)
         print(data[clustering.labels_ == i].index.tolist())
         print('------------------------')
+
+
+def find_clusters_kmedoids(data_preprocessed, nr_clusters):
+    # apply elbow and shilouette method to preprocessed dataframe to find the optimal number of clusters
+    wcss = [] 
+    silhouette_avg = []
+    for i in range(2, nr_clusters): 
+        kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+        kmeans.fit(data_preprocessed) 
+        # wcss score
+        wcss.append(kmeans.inertia_)
+        # silhouette score
+        silhouette_avg.append(silhouette_score(data_preprocessed, kmeans.fit_predict(data_preprocessed)))
+
+    # subplot with two plots
+    fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+    xticks = range(1, nr_clusters)
+    # plot wcss score
+    ax[0].plot(range(2, nr_clusters), wcss, marker='o', linestyle='--')
+    # plot silhouette score
+    ax[1].plot(range(2, nr_clusters), silhouette_avg, marker='o', linestyle='--')
+    # make x-labels
+    ax[0].set_xlabel('Number of clusters')
+    ax[1].set_xlabel('Number of clusters')
+    # make y-labels
+    ax[0].set_ylabel('WCSS')
+    ax[1].set_ylabel('Silhouette score')
+    # make title
+    ax[0].set_title('Elbow Method')
+    ax[1].set_title('Silhouette Method')
+    # set xticks
+    ax[0].set_xticks(xticks)
+    ax[1].set_xticks(xticks)
+    # show plot
+    plt.show()
+
+
+def silhouette_plot(data_preprocessed, cluster_sizes):
+    fig, ax = plt.subplots(1,4, figsize=(15,8))
+    q = 0
+    for i in cluster_sizes:
+        # Create KMeans instances for different number of clusters
+        km = KMeans(n_clusters=i, init='k-means++', n_init=10, max_iter=100, random_state=42)
+        # Create SilhouetteVisualizer instance with KMeans instance
+        # Fit the visualizer
+        visualizer = SilhouetteVisualizer(km, colors='yellowbrick', ax=ax[q])
+        # title for subplot
+        ax[q].set_title('Silhouette plot for '+str(i)+' clusters')
+        visualizer.fit(data_preprocessed)
+        q += 1
